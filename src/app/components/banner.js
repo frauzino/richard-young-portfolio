cfffc02585773068c4da3d2e75ced67006c4426d
Richard Young <richarddfyoung@gmail.com>
2024-01-18 18:44:55 -0500

    added 'react-parallax-mouse' made banner parallax

--- src/app/components/banner.js
+++ src/app/components/banner.js
@@ -1,15 +1,31 @@
 import styles from './banner.module.scss'
+import { MouseParallaxContainer, MouseParallaxChild } from "react-parallax-mouse";
 
 export function Banner(props) {
   const header = props.header
   const content = props.content
 
   return (
-    <div className={styles.banner}>
-      <div className={styles.banner__text_box}>
-        <h1 className={styles.text_box__header}>{header}</h1>
-        <p className={styles.text_box__content}>{content}</p>
-      </div>
+    <div className={styles.banner_container}>
+      <MouseParallaxContainer
+        className={styles.banner}
+        resetOnLeave
+      >
+        <MouseParallaxChild
+          className={styles.parallax_banner}
+          factorX={.03}
+          factorY={.01}
+        />
+        <MouseParallaxChild
+          factorX={0.05}
+          factorY={0.05}
+        >
+          <div className={styles.banner__text_box}>
+            <h1 className={styles.text_box__header}>{header}</h1>
+            <p className={styles.text_box__content}>{content}</p>
+          </div>
+        </MouseParallaxChild>
+      </MouseParallaxContainer>
     </div>
   )
 }
