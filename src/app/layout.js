45c5c81d24ab53aaf6d2cab37b684c5803fe484c
Richard Young <richarddfyoung@gmail.com>
2024-05-03 13:38:54 -0400

    updated open graph meta tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,22 @@ import { Navbar } from './components/navbar'
 
 export const metadata = {
   title: 'Richard Young',
-  description: 'Portfolio for web developer, Richard Young'
+  description: 'Portfolio for web developer, Richard Young',
+  openGraph: {
+    title: 'Richard Young',
+    description: 'Portfolio for web developer, Richard Young',
+    url: 'http://richardyoungdev.com/',
+    siteName: 'Richard Young Portfolio',
+    images: [
+      {
+        url: 'homepage_screenshot.png',
+        width: 1200,
+        height: 630,
+        alt: 'Richard Young Portfolio',
+      },
+    ],
+    type: 'website',
+  },
 }
 
 export default function RootLayout({ children }) {
